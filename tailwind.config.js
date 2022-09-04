584bf002e4f9f5417a25d644e569a3d10cbc35e6
truong <quangtruong.tdr012@gmail.com>
2022-09-04 12:56:53 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blackx: "#1A2530",
+        bluex: "#5B9EE1",
+        grayx: "#707B81",
+        "blackx-shape": "#161F28",
+      },
+    },
+  },
+  plugins: [],
+};
